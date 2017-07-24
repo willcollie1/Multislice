40b25a8eca1b47c5066257501f25515143f44d8f
willcollie1 <willcollie123@gmail.com>
2017-07-24 14:33:40 +0100

    update

--- program.py
+++ program.py
@@ -1,44 +1,46 @@
-#This program takes the output file from "multislice.c" containing intensity values and turns them into a greyscale image. 
+#This program uses the output file from "multislice.c" to create a greyscale image of the unit cell.
 
-import subprocess #Subprocess used to run multislice program from python script
-import os 
-from PIL import Image #Python Image Libary used to create greyscale image
+import subprocess
+import os #Run linux commands from Python program
+from PIL import Image #Image Processing
 
+#Unzip the potential file (if it isn't already unzipped)
+unzipping = os.system("bzip2 -dk 'Example Potential file .pot_fmt'.bz2")
 
-# Running the multislice program
+#Uses Linux sort to reorder the input file to row major format.
+reordering = os.system("sort -n -k3,3 -k2,2 -k1,1 < potential.txt > output1.txt")  
+
+#Runs the multislice C program
 var = os.system("./multislice")
-print(var)
 
 
-# Reading in parameters from c program output
+#Reads in array parameters from multislice
 parameters = open("size.txt").readlines()
 height = int(parameters[0])
 width = int(parameters[1])
 size = height*int(width)
 
-# Open output file from multislice and read in the intensity values
+#Read in intensity values from multislice.c and create a list
 List = open("output.txt").readlines()
 
-# Find the maximum and minimum intensity value
 maximum = max(List)
 minimum = min(List)
 
-# Normalise the data so that the intensity values fall between 0 and 255 (8 bits).
+
+#Normalise intensity values so that they fill the range 0-255 (8 bit binary)
 for i in range (0,size):
     List[i] =  255*(float(List[i])-float(minimum))/(float(maximum) - float(minimum))
     List[i] = int(List[i])
-    print(List[i])
 
 
 
+#Create greyscale image with the given intensity values.
 newim = Image.new('L', (height,width))
 pixels1 = newim.load()
-
-# Set pixels equal to their corresponding intensity values. 
 for i in range (0,(height-1)):
     for j in range(0,(width-1)):
-        l = List[i+height*j]
+        l = List[i+height*j] 
  
-        pixels1[i,j] = (l)
+        pixels1[i,j] = (l) 
 
 newim.save("newim", "JPEG")
